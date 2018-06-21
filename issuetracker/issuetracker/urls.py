cba38c06f8a05ca746233e0bcc25c707ea5c08e0
Eugeniusz Gilewski <egilewski@egilewski.com>
2018-06-21 21:52:09 +0200

    t3 Squashed commit of the following:
    commit f71b5e199175aadc67ef4e06e397c0ccf27d8dec
    Author: Eugeniusz Gilewski <egilewski@egilewski.com>
    Date:   Thu Jun 21 21:47:44 2018 +0200
    
        t3 Add Issue, IssueCategory, IssueStatus to admin
    
    commit d274af2d03030f180778c5fb7b20f3764c7d3f0c
    Author: Eugeniusz Gilewski <egilewski@egilewski.com>
    Date:   Thu Jun 21 19:41:09 2018 +0200
    
        t3 Move Django Admin interface to /

--- issuetracker/issuetracker/urls.py
+++ issuetracker/issuetracker/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
+from django.utils.translation import ugettext_lazy
+
+
+admin.site.site_header = ugettext_lazy("Issue Tracker")
+admin.site.index_title = ugettext_lazy("Issues and related entities")
+admin.site.site_url = ''
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
+    path('admin/', RedirectView.as_view(url='/')),
 ]
